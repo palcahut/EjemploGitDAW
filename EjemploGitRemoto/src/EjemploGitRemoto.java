de8195a64b248b04ceb23998762c2b8c47609f6b
Pedro Alcahut <palcahut@gmail.com>
2024-04-16 17:32:03 +0200

    se añade suma

--- EjemploGitRemoto/src/EjemploGitRemoto.java
+++ EjemploGitRemoto/src/EjemploGitRemoto.java
@@ -3,7 +3,12 @@ public class EjemploGitRemoto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("4+3="+suma(4,3));
 
+	}
+	
+	static int suma(int a, int b) {
+		return a + b;		
 	}
 
 }
