59833db1303e72f1adc9b59a2d1d7122d44fb35a
Pedro Alcahut <palcahut@gmail.com>
2024-04-16 17:33:27 +0200

    se añade resta

--- EjemploGitRemoto/src/EjemploGitRemoto.java
+++ EjemploGitRemoto/src/EjemploGitRemoto.java
@@ -3,7 +3,12 @@ public class EjemploGitRemoto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("4-3="+resta(4,3));
 
+	}
+	
+	static int resta (int a, int b) {
+		return a-b;
 	}
 
 }
